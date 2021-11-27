1a8bfc2e47ebcaac9bf92ee3f747d25860ac92cc
arjun6487 <91003539+arjun6487@users.noreply.github.com>
2021-11-27 19:56:16 +0530

    Create AreaOfFigure.java

---
+++ AreaOfFigure.java
@@ -0,0 +1,42 @@
+abstract class Figure{
+	double dimension1;
+	double dimension2;
+	
+	Figure(double x, double y){
+		dimension1 = x;
+		dimension2 = y;
+	}
+	abstract double area();
+}
+class Rectangle extends Figure{
+	Rectangle(double x, double y){
+		super(x,y);
+	}
+	double area() {
+		System.out.println("Area of rectangle is :");
+		return dimension1*dimension2;
+	}
+}
+class Triangle extends Figure{
+	Triangle(double x, double y){
+		super(x,y);
+	}
+	double area() {
+		System.out.println("Area for triangle is :");
+		return (dimension1*dimension2)/2;
+	}
+}
+public class FindArea {
+public static void main(String[] args) {
+	Figure fig;
+	Rectangle r = new Rectangle(9,5);
+	Triangle t = new Triangle(10, 8);
+	
+	
+	fig = r;
+	System.out.println("Area of rectangle is :" + fig.area());
+	fig = t;
+	System.out.println("Area of rectangle is :" + fig.area());
+	
+}
+}
